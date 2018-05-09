8eaa0e933a5781940380efcdf768f4593d634430
TheKilogram <jonathanbauman8@gmail.com>
2018-05-09 16:57:03 -0400

    Initialization and collider addition
    Initialization and collider addition to project

---
+++ ProjectOslo/Assets/swordScripts/test/testSwordCut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testSwordCut : MonoBehaviour {
+  
+    //when sword collides
+    void OnCollisionEnter(Collision col)
+    {
+        //when sword collids with a cuttable object
+        if (col.gameObject.transform.tag == "cutable")
+        {
+            //print("hello");  //you can uncomment for debuging
+            Mesh mesh = col.gameObject.GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            int i = 0;
+            while (i < vertices.Length)
+            {
+                vertices[i] += Vector3.up * Time.deltaTime;
+                i++;
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+        }
+        
+    }
+}
